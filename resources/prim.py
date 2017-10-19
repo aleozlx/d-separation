d42eb776228cd430dfa93e5d566e41c482b8f403
Alex Yang <aleozlx@gmail.com>
2017-10-19 17:21:17 -0500

    checkpoint

--- resources/prim.py
+++ resources/prim.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
 import numpy as np
 import unittest, random, itertools
-from queue import PriorityQueue
+from heapq import heapify
 
 G1 = [{2:1}, {2:1}, {3:1, 7:1}, {5:1}, {5:1}, {6:1}, {}, {5:1}]
 
 
 def get_undirected_graph(G):
-    H = [set() for u in G]
+    H = [dict() for u in G]
     for u, V in enumerate(G):
-        for v in V:
-            H[u].add(v)
-            H[v].add(u)
+        for v,d in V.items():
+            H[u][v] = H[v][u] = d
     return H
 
-# def prim(G,s):
-#     distance = np.array([1000]*len(G))
-#     predecessor = -np.ones_like(G)
-#     q = PriorityQueue()
-#     distance[s] = 0
-#     q.buildHeap([(distance[vi],v) for vi, v in enumerate(G)])
-#     while not q.empty():
-#         du, u = q.get()
-#         for v in u.keys():
-#             newCost = currentVert.getWeight(nextVert)
-#             if nextVert in q and newCost<nextVert.getDistance():
-#                 nextVert.setPred(currentVert)
-#                 nextVert.setDistance(newCost)
-#                 q.decreaseKey(nextVert,newCost)
+def prim(G,s):
+    distance = [1000]*len(G)
+    predecessor = -np.ones_like(G)
+    distance[s] = 0
+    q = heapify([(distance[u],(u,V)) for u, V in enumerate(G)])
+    while not q.empty():
+        (du, (u, V)) = q.get()
+        print(u)
+        for v, weight in V.items():
+            newCost = weight
+            if v in q.queue and newCost<distance[v]:
+                predecessor[v] = u
+                distance[v] = newCost
+                q.decreaseKey(nextVert,newCost)
 
-# prim(G1,0)
-
-print(get_undirected_graph(G1))
+prim(get_undirected_graph(G1),0)
